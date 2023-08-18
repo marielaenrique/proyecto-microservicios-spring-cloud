0585d3ffed04ad09232d85bf3daa03c82841857c
marienrique <mariela.enrique@gmail.com>
2023-08-17 23:47:08 -0300

    Subiendo load-balancer

--- catalog-service/src/main/java/com/dh/catalogservice/feign/IMovieClient.java
+++ catalog-service/src/main/java/com/dh/catalogservice/feign/IMovieClient.java
@@ -1,6 +1,7 @@
 package com.dh.catalogservice.feign;
 
 import com.dh.catalogservice.model.Movie;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @FeignClient(name = "movie-service")
+@LoadBalancerClient(value = "movie-service", configuration = FeignConfiguration.class)
 public interface IMovieClient {
     @GetMapping("/api/v1/movies/{genre}")
     ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre);
