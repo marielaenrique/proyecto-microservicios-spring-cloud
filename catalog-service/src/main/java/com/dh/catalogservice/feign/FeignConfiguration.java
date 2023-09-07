e30955782f9b4f62c5c23408863a72222c5afa7a
marienrique <mariela.enrique@gmail.com>
2023-09-06 22:45:14 -0300

    Agregando configuration annotation

--- catalog-service/src/main/java/com/dh/catalogservice/feign/FeignConfiguration.java
+++ catalog-service/src/main/java/com/dh/catalogservice/feign/FeignConfiguration.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+@Configuration
 public class FeignConfiguration {
 
     @Bean
